ffe6cbb59ac3bb9e053ba951281b95274673f5cd
MaitreRenard18 <maitre.renardowo@gmail.com>
2024-02-09 16:18:49 +0100

    Circle tilemap generation

--- scripts/tilemap_earth_generation.gd
+++ scripts/tilemap_earth_generation.gd
@@ -1,6 +1,6 @@
 extends TileMap
 
-@export var radius: int = 10;
+@export var radius := 32;
 
 var source_id := 0;
 
@@ -9,14 +9,21 @@ func _ready():
 	var height_noise = FastNoiseLite.new();
 	height_noise.noise_type = height_noise.TYPE_SIMPLEX;
 	
-	for x in range(-radius, radius + 1):
-		var theta := acos(x / radius);
-		var y := sin(theta) * radius;
+	for x in range(-radius, 1):
+		var theta := acos(float(x) / float(radius));
+		var y := int(round(sin(theta) * radius));
 		
-		print(x, " ", theta, " ", y)
-		set_cell(0, Vector2i(x, y), source_id, Vector2i(0, 0));
-		set_cell(0, Vector2i(x, -y), source_id, Vector2i(0, 0));
-
+		var next_theta = acos(float(x + 1) / float(radius));
+		var next_y := int(round(sin(next_theta) * radius));
+		
+		for i in range(max(abs(next_y - y), 1)):
+			if next_y < y:
+				i *= -1
+			
+			set_cell(0, Vector2i(x, y + i), source_id, Vector2i(0, 0));
+			set_cell(0, Vector2i(x, -y - i), source_id, Vector2i(0, 0));
+			set_cell(0, Vector2i(-x, y + i), source_id, Vector2i(0, 0));
+			set_cell(0, Vector2i(-x, -y - i), source_id, Vector2i(0, 0));
 
 func _process(delta):
 	pass
