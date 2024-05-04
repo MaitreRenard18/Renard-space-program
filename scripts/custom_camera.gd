920075549a1ff3636d25fd572f471f5dcde02f88
MaitreRenard18 <maitre.renardowo@gmail.com>
2024-05-04 15:06:52 +0200

    Created Earth class

--- scripts/custom_camera.gd
+++ scripts/custom_camera.gd
@@ -2,13 +2,35 @@
 class_name CustomCamera2D
 extends Camera2D
 
+@onready var bodies: Array = []
+@onready var displays: Array = []
+
+var planet_renderer_shader = preload("res://shaders/planet_renderer.gdshader")
+
+
 func _process(_delta):
-	var display = $ColorRect
+	for display in displays:
+		display.scale = Vector2(1 / zoom.x, 1 / zoom.y)
+		display.size = get_viewport_rect().size
+		display.get_material().set_shader_parameter("camera_zoom", zoom)
+		
+		display.position = -display.size * display.scale / 2.0
+		display.get_material().set_shader_parameter("camera_top_left_position", display.global_position.rotated(-global_rotation))
+		display.get_material().set_shader_parameter("camera_rotation", global_rotation)
+
+
+# TODO: Replace earth with a generic body class
+func add_body(body: Earth):
+	bodies.append(body)
 	
-	display.scale = Vector2(1 / zoom.x, 1 / zoom.y)
-	display.size = get_viewport_rect().size
-	display.get_material().set_shader_parameter("camera_zoom", zoom)
+	var display: ColorRect = ColorRect.new()
+	display.material = ShaderMaterial.new()
+	display.material.shader = planet_renderer_shader
+
+	display.material.set_shader_parameter("height_map", body.height_map)
+	display.material.set_shader_parameter("planet_position", body.position)
+	display.material.set_shader_parameter("planet_radius", body.planet_radius)
+	display.material.set_shader_parameter("noise_strenght", body.noise_strenght)
 	
-	display.position = -display.size * display.scale / 2.0
-	display.get_material().set_shader_parameter("camera_top_left_position", display.global_position)
-	display.get_material().set_shader_parameter("camera_rotation", global_rotation)
+	displays.append(display)
+	add_child(display)
---
+++ scripts/earth.gd
@@ -0,0 +1,104 @@
+class_name Earth
+extends Node2D
+
+@export_category("Generation Settings")
+@export var world_seed: int = -1
+@export var planet_radius: float
+@export var collision_resolution: int = 64
+var collision_shape: CollisionPolygon2D
+
+@export_category("Height Map Settings")
+var height_map: NoiseTexture2D
+var height_map_image: Image
+@export var texture_size: int
+@export var frequency: float
+@export var noise_strenght: float
+
+@onready var current_camera: CustomCamera2D = get_viewport().get_camera_2d()
+
+var atmosphere_shader: Shader = preload("res://shaders/atmosphere.gdshader")
+var planet_shadow_shader: Shader = preload("res://shaders/planet_shadow.gdshader")
+
+
+func map(value: float, in_min: float, in_max: float, out_min: float, out_max: float) -> float:
+	return (value - in_min) / (in_max - in_min) * (out_max - out_min) + out_min
+
+
+func get_noise_value(theta: float) -> float:
+	var x: int = int(map(cos(theta), -1, 1, 0, texture_size - 1))
+	var y: int = int(map(sin(theta), -1, 1, 0, texture_size - 1))
+
+	return height_map_image.get_pixel(x, y).r
+
+
+# TODO: fix random holes in the terrain
+func get_terrain_height(theta: float) -> float:
+	if get_biome(theta) == "water":
+		return planet_radius + noise_strenght * .45
+
+	return get_noise_value(theta) * noise_strenght + planet_radius
+
+
+func get_biome(theta: float) -> String:
+	var noise_value: float = get_noise_value(theta)
+
+	if noise_value > 0.5:
+		return "grass"
+	elif noise_value > 0.45:
+		return "sand"
+	else:
+		return "water"
+
+
+func _ready():
+	# Create the height map
+	height_map = NoiseTexture2D.new()
+	height_map.height = texture_size
+	height_map.width = texture_size
+	height_map.noise = FastNoiseLite.new()
+	height_map.noise.seed = world_seed
+	height_map.noise.frequency = frequency
+	height_map.noise.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
+	height_map.generate_mipmaps = false
+	
+	await height_map.changed
+	height_map_image = height_map.get_image()
+
+	print(height_map_image)
+
+	# Set up rendering
+	current_camera.add_body(self)
+
+	# Generate colision
+	# TODO: Remove collision with water
+	collision_resolution = max(collision_resolution, 3)
+	collision_shape = CollisionPolygon2D.new()
+	var collision_points: PackedVector2Array = []
+
+	for i in range(collision_resolution):
+		var theta = i * 2 * PI / collision_resolution
+		var height = get_terrain_height(theta)
+		var x = cos(theta) * height
+		var y = sin(theta) * height
+		collision_points.append(Vector2(x, y))
+	
+	collision_shape.set_polygon(collision_points)
+	add_child(collision_shape)
+
+	# Set up atmosphere
+	var atmosphere: ColorRect = ColorRect.new()
+	atmosphere.size = Vector2(2.75 * planet_radius, 2.75 * planet_radius)
+	atmosphere.position = -atmosphere.size / 2
+	atmosphere.material = ShaderMaterial.new()
+	atmosphere.material.shader = atmosphere_shader
+	atmosphere.z_index = -1
+	add_child(atmosphere)
+
+	# Set up shadow
+	var shadow: ColorRect = ColorRect.new()
+	shadow.size = Vector2(2.75 * planet_radius, 2.75 * planet_radius)
+	shadow.position = -shadow.size / 2
+	shadow.material = ShaderMaterial.new()
+	shadow.material.shader = planet_shadow_shader
+	shadow.z_index = 1
+	add_child(shadow)
