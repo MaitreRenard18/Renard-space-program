18742a23ca2d17bdfa25b5b0cb65812a200ab869
MaitreRenard18 <maitre.renardowo@gmail.com>
2024-05-18 13:38:42 +0200

    Optimization

--- scripts/shaders_handler.gd
+++ scripts/shaders_handler.gd
@@ -1,14 +1,23 @@
 extends Node
 
+var screen_size: Vector2
+@onready var current_camera: Camera2D = get_viewport().get_camera_2d()
+
+
+func _ready():
+	get_tree().root.size_changed.connect(update_screen_size)
+	update_screen_size()
+
 
 func _process(_delta):
-	var screen_size: Vector2 = get_tree().root.size
-	var current_camera: Camera2D = get_viewport().get_camera_2d()
-	
 	var camera_position: Vector2 = current_camera.global_position.rotated(-current_camera.global_rotation) - screen_size / 2 / current_camera.zoom
 	
-	RenderingServer.global_shader_parameter_set("screen_size", screen_size)
 	RenderingServer.global_shader_parameter_set("camera_zoom", current_camera.zoom)
 	RenderingServer.global_shader_parameter_set("camera_top_left_position", camera_position)
 	RenderingServer.global_shader_parameter_set("camera_rotation", current_camera.global_rotation)
+
+
+func update_screen_size():
+	screen_size = get_tree().root.size
+	RenderingServer.global_shader_parameter_set("screen_size", screen_size)
 
